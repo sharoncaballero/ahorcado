cc96053f011e8c777c5495acfa9364168233f935
Sharon Caballero <scaballero@fi.uba.ar>
2021-02-21 00:40:10 -0300

    Modularizando creacion de palabras

--- gestor_archivos/resumir_texto.py
+++ gestor_archivos/resumir_texto.py
@@ -43,3 +43,17 @@ def mezclar_archivos():
 
     generar_archivo_de_palabras(sorted(list(palabras)),"configuracion/palabras.txt")
     return tabla_de_longitud,len(palabras)
+
+def crear_archivo_de_palabras():
+    palabras = dividir_texto_en_palabras_correctas("textos/Cuentos.txt")
+    generar_archivo_de_palabras(palabras, "configuracion/palabras_texto_1.txt")
+
+    palabras = dividir_texto_en_palabras_correctas("textos/La araña negra - tomo 1.txt")
+    generar_archivo_de_palabras(palabras, "configuracion/palabras_texto_2.txt")
+
+    palabras = dividir_texto_en_palabras_correctas("textos/Las 1000 Noches y 1 Noche.txt")
+    generar_archivo_de_palabras(palabras, "configuracion/palabras_texto_3.txt")
+
+    tabla_de_longitud, total = mezclar_archivos()
+    print(tabla_de_longitud, total)
+    return crear_archivo_de_palabras
--- main.py
+++ main.py
@@ -1,17 +1,6 @@
-from gestor_archivos.resumir_texto import dividir_texto_en_palabras_correctas, generar_archivo_de_palabras, mezclar_archivos
+from gestor_archivos.resumir_texto import dividir_texto_en_palabras_correctas, generar_archivo_de_palabras, mezclar_archivos, crear_archivo_de_palabras
 from juego_ahorcado.ahorcado import jugar
 
-palabras = dividir_texto_en_palabras_correctas("textos/Cuentos.txt")
-generar_archivo_de_palabras(palabras, "configuracion/palabras_texto_1.txt")
-
-palabras = dividir_texto_en_palabras_correctas("textos/La araña negra - tomo 1.txt")
-generar_archivo_de_palabras(palabras, "configuracion/palabras_texto_2.txt")
-
-palabras = dividir_texto_en_palabras_correctas("textos/Las 1000 Noches y 1 Noche.txt")
-generar_archivo_de_palabras(palabras, "configuracion/palabras_texto_3.txt")
-
-
-tabla_de_longitud,total = mezclar_archivos()
-print(tabla_de_longitud,total)
+tabla_de_longitud = crear_archivo_de_palabras()
 
 jugar(tabla_de_longitud)
